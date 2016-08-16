40eecd25812e68db82b265ab3b1530d524e8e504
goodbedford <bedford@goodbedford.com>
2016-08-16 13:21:29 -0700

    fixed rwd and moved lnk mixin to own file

--- src/js/app.js
+++ src/js/app.js
@@ -1,35 +1,43 @@
-
-var productHeader = $(".product__header");
-var menuLinks = $(".menu__link");
+"use strict";
+var $productHeader = $(".product__header");
+var $menuLinks = $(".menu__link");
 var activate = activate;
 var animation = {
-  rock: "ani-rock",
-  shake: "ani-shake",
-  border: "ani-border-bottom"
+    rock: "ani-rock",
+    shake: "ani-shake",
+    border: "ani-border-bottom"
 };
+var activate = activate;
+var productHoverHandler = productHoverHandler;
+var productHoverRemoveHandler = productHoverRemoveHandler;
+var menuLinkHoverHandler = menuLinkHoverHandler;
+var menuLinkHoverRemoveHandler = menuLinkHoverRemoveHandler;
 
 activate();
 
 function activate() {
-  productHeader.hover( productHoverHandler, productHoverRemoveHandler, animation );
-  menuLinks.hover(menuLinkHoverHandler, menuLinkHoverRemoveHandler, animation)
+    $productHeader.hover(productHoverHandler, productHoverRemoveHandler, animation);
+    $menuLinks.hover(menuLinkHoverHandler, menuLinkHoverRemoveHandler, animation);
 }
+
 function productHoverHandler(event) {
-  console.log("added hover", animation.shake);
-  $(this).addClass(animation.shake);
+    console.log("added",event.type, animation.shake);
+    $(this).addClass(animation.shake);
 }
+
 function productHoverRemoveHandler(event) {
-  console.log("removed hover", animation.shake);
-  $(this).removeClass(animation.shake);
+    console.log("removed", event.type, animation.shake);
+    $(this).removeClass(animation.shake);
 }
 
 function menuLinkHoverHandler(event) {
-  console.log("added hover", animation.border);
-  console.log($(this));
-  var div = $("<div>");
-  $(this).append(div.addClass(animation.border));
+    console.log("added", event.type, animation.border);
+    console.log($(this));
+    var $div = $("<div>");
+    $(this).append($div.addClass(animation.border));
 }
+
 function menuLinkHoverRemoveHandler(event) {
-  $(this).find("div").removeClass(animation.border);
-   console.log("removed hover", animation.border, $(this).find("div"));
+    $(this).find("div").removeClass(animation.border);
+    console.log("removed", event.type, animation.border, $(this).find("div"));
 }
